6132417e1280680d720e2b214e20f1fea4142e96
oguzBatur <sariozbatur@gmail.com>
2022-10-02 14:53:06 +0300

    changed env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const ALCHEMY_API_KEY = "Mf_-Y0XJnYNy4PVJ_ag2QJH662dMzvG4";
-const GOERLI_PRIVATE_KEY = "fba742243e2b7fa2478af69272ffac26fbd8d82983008f391caa9ffb92f427d3";
-
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   etherscan: {
@@ -12,8 +9,8 @@ const config: HardhatUserConfig = {
   networks: {
 
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY],
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
     }
 
   },
